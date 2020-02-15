3a3a149d027d7b8e21246f735c6792b66f6ea997
Theerapat Chawannakul <theerapat.cha@gmail.com>
2020-02-16 03:43:35 +0700

    added user api

--- app/controllers/user.js
+++ app/controllers/user.js
@@ -5,6 +5,16 @@
 const userStore = require('../stores/user-store');
 
 exports.create = function (app) {
+    app.get('/api/users/:id', (req, res) => {
+        const user = userStore.get(req.params.id)
+        if (user) {
+            res.status(200).json(user);
+        } else {
+            res.status(404).json({
+                error: "not found"
+            })
+        }
+    })
     app.get('/api/users/me', (req, res) => {
         const user = userStore.getOrCreateUserByUsername(req.headers.username)
         res.status(200).json(user);
