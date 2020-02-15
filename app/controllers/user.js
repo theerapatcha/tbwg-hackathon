47d26d37c8ac388933e37cbed107f932af7e80db
Theerapat Chawannakul <theerapat.cha@gmail.com>
2020-02-16 03:46:42 +0700

    fix

--- app/controllers/user.js
+++ app/controllers/user.js
@@ -5,6 +5,10 @@
 const userStore = require('../stores/user-store');
 
 exports.create = function (app) {
+    app.get('/api/users/me', (req, res) => {
+        const user = userStore.getOrCreateUserByUsername(req.headers.username)
+        res.status(200).json(user);
+    })
     app.get('/api/users/:id', (req, res) => {
         const user = userStore.get(req.params.id)
         if (user) {
@@ -15,8 +19,5 @@ exports.create = function (app) {
             })
         }
     })
-    app.get('/api/users/me', (req, res) => {
-        const user = userStore.getOrCreateUserByUsername(req.headers.username)
-        res.status(200).json(user);
-    })
+
 };
